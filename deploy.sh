cd2a31748d08271d21ba1d21cfb7092030a3da2a
angelmarrugo <monoremix_@hotmail.com>
2020-12-01 07:56:52 -0600

    update production

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:angelmarrugo/MenuSoccer.git master:gh-pages
+ git push -f git@github.com:angelmarrugo/RepoMenuSoccer.git master:gh-pages
 
 cd -
